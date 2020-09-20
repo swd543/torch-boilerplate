feaa1829e430788820c57a64c3d358876e512321
Swapneel <swd543@gmail.com>
2020-09-20 13:07:02 +0200

    Moved model to dedicated file

---
+++ utils/model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# %%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class SomeNet(nn.Module):
+    """Some Information about SomeNet"""
+    def __init__(self, input_shape=(32,32), output_shape=100):
+        super(SomeNet, self).__init__()
+        self.input_shape=input_shape
+        self.output_shape=output_shape
+
+        self.i=nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1),
+            nn.ReLU()
+        )
+        self.c=nn.Sequential(
+            nn.Conv2d(16, 32, 3),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3),
+            nn.ReLU(),
+            # nn.BatchNorm2d(128),
+        )
+        self.l=nn.Sequential(
+            nn.Linear(73728, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(2048, self.output_shape),
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        x=self.i(x)
+        x=self.c(x)
+        x=x.view(x.size(0),-1)
+        x=self.l(x)
+        return x
+
+if __name__ == "__main__":
+    model=SomeNet()
+    print(model)
